ff2f001752f3cb5e2940451b27911079a56c2f34
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-30 22:06:02 -0600

    Overiding functions

--- Tutorial14/Source.cpp
+++ Tutorial14/Source.cpp
@@ -3,11 +3,43 @@ using namespace std;
 
 class Object
 {
-
+public:
+	virtual void BeginPlay() {
+		cout << "Object BeginPlay() called. \n";
+	}
 };
 
-int main() {
+class Actor : public Object
+{
+public:
+	virtual void BeginPlay() {
+		cout << "Actor BeginPlay() called. \n";
+		Object::BeginPlay();
+	}
+};
+
+class Pawn : public Actor {
+public:
+	void BeginPlay() {
+		cout << "Pawn BeginPlay() called. \n";
+		Object::BeginPlay();
+		Actor::BeginPlay();
+	}
+};
+
+int main() {/*
+	Object* obj = new Object;
+	obj->BeginPlay();
+
+	Actor* act = new Actor;
+	act->BeginPlay();*/
+
+	Pawn* pawn = new Pawn;
+	pawn->BeginPlay();
 
 
+	delete pawn;
+	//delete obj;/*
+	//delete act;*/
 	system("pause");
 }
